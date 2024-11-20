0d033ba4203c85e3b5b3ea3dea7e6cf68b84354a
Gallasko <gallasko@gmail.com>
2024-11-20 03:13:43 +0100

    Inventory system

---
+++ src/GameOff/inventory.cpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "inventory.h"
+
+namespace pg
+{
+    void InventorySystem::onEvent(const GainItem& event)
+    {
+        auto item = event.item;
+
+        if (item.stacksize == 1)
+        {
+            items[item.type].push_back(item);
+            return;
+        }
+
+        auto it = std::find(items[item.type].rbegin(), items[item.type].rend(), item);
+
+        if (it != items[item.type].rend())
+        {
+            // A stack size of -1 means it is infinite
+            if (it->stacksize == -1)
+            {
+                it->nbItems += item.nbItems;
+                return;
+            }
+
+            int remainingSize = it->nbItems - it->stacksize;
+
+            if (remainingSize > 0)
+            {
+                if (remainingSize > item.nbItems)
+                {
+                    it->nbItems += item.nbItems;
+                    return; 
+                }
+                else
+                {
+                    it->nbItems += remainingSize;
+                    item.nbItems -= remainingSize;
+                }
+            }
+        }
+
+        do
+        {
+            auto itemToPush = item;
+
+            if (item.nbItems >= item.stacksize)
+            {
+                itemToPush.nbItems = item.stacksize;
+            }
+
+            items[item.type].push_back(item);
+
+            item.nbItems -= item.stacksize;
+        }
+        while (item.nbItems > 0);
+
+    }
+
+    void InventorySystem::onEvent(const LoseItem& event)
+    {
+        auto item = event.item;
+
+        auto it = std::find(items[item.type].rbegin(), items[item.type].rend(), item);
+
+        if (it == items[item.type].rend())
+        {
+            LOG_ERROR("INVENTORY", "Requested removing of an item that is not in the inventory: " << item.name);
+            return;
+        }
+
+        it->nbItems -= 1;
+
+        if (it->nbItems <= 0)
+        {
+            items[item.type].erase(std::next(it).base());
+        }
+    }
+}
---
+++ src/GameOff/inventory.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "ECS/system.h"
+
+namespace pg
+{
+    enum class ItemType : uint8_t
+    {
+        Weapon = 0,
+        Armor,
+        Consomable,
+        SkillBook,
+        SkillXp,
+        Material,
+        SpellStone,
+        Key
+    };
+
+    enum class ItemRarity : uint8_t
+    {
+        Common = 0,
+        Uncommon,
+        Rare,
+        Epic,
+        Legendary,
+        Mythic,
+        Unique
+    };
+
+    struct Item
+    {
+        /** Name of the item */
+        std::string name;
+        
+        /** Type of the item */
+        ItemType type;
+
+        /** Number of items in a stack */
+        int stacksize = 1;
+        /** Current number of items in this stack */
+        size_t nbItems;
+
+        /** Rarity of the item */
+        ItemRarity rarity;
+
+        /**
+         * Map of all the attributes of an item
+         * 
+         * Exemple of attributes available
+         * ConsumedUponUsed -> bool
+         * StatsBoost (for weapons and armor) -> <"MATK", "5">
+         * Spell -> "Spell1;Spell2;Spell3" (spellname string in csv format)
+         */
+        std::unordered_map<std::string, ElementType> attributes;
+        
+        // Todo maybe add a pointer to a character (For weapons and armors)
+
+        /** Description of the item */
+        std::string description = "An item";
+    };
+
+    struct GainItem
+    {
+        GainItem(const Item& item) : item(item) {}
+
+        Item item;
+    };
+
+    struct LoseItem
+    {
+        LoseItem(const Item& item) : item(item) {}
+
+        Item item;
+    };
+
+    struct UseItem
+    {
+
+    };
+
+    struct InventorySystem : public System<Listener<GainItem>, Listener<LoseItem>, StoragePolicy>
+    {
+        void onEvent(const GainItem& event);
+        void onEvent(const LoseItem& event);
+
+        std::unordered_map<ItemType, std::vector<Item>> items;
+    };
+}
